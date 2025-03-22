e987be7e739fd65c8cb001a81edbca01274d174d
alewand <lewandowskiamadeusz2004@gmail.com>
2025-03-22 10:04:29 +0100

    Created user model.

---
+++ server/src/db/models/user.ts
@@ -0,0 +1,87 @@
+import { DataType, Table, Model, Column } from "sequelize-typescript";
+
+export enum Gender {
+  M = "male",
+  F = "female",
+}
+
+export enum Role {
+  ADMIN = "admin",
+  USER = "user",
+}
+
+@Table({
+  tableName: "users",
+})
+export class User extends Model {
+  @Column({
+    primaryKey: true,
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+  })
+  declare userId: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      len: [3, 20],
+    },
+  })
+  declare nickname: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      len: [2, 20],
+    },
+  })
+  declare name: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      len: [2, 50],
+    },
+  })
+  declare surname: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  })
+  declare email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  declare password: string;
+
+  @Column({
+    type: DataType.ENUM(...Object.values(Gender)),
+    allowNull: false,
+  })
+  declare gender: Gender;
+
+  @Column({
+    type: DataType.ENUM(...Object.values(Role)),
+    allowNull: false,
+    defaultValue: Role.USER,
+  })
+  declare role: Role;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  })
+  declare isActivate: boolean;
+}
