0e284e664da0829a832b500f1fb7792b66fafe0f
hkaczyns <01187235@pw.edu.pl>
2025-04-17 03:42:01 +0200

    Modified the question controller to utilize the QuestionController class

--- server/src/controllers/question.controller.ts
+++ server/src/controllers/question.controller.ts
@@ -1,17 +1,22 @@
 import { Request, Response } from "express";
 import { getRandomQuestions, saveAnswer } from "../services/question.service";
 
-export const getRandom = (req: Request, res: Response) => {
-  const count = Number(req.query.count) || 3;
-  const randomQuestions = getRandomQuestions(count);
-  res.json(randomQuestions);
-};
-
-export const postAnswer = (req: Request, res: Response) => {
-  const { userId, questionId, answer } = req.body;
-  if (!userId || !questionId || !answer) {
-    return res.status(400).json({ error: "Brakuje danych" });
+export class QuestionController {
+  static async getRandom(req: Request, res: Response): Promise<void> {
+    const count = Number(req.query.count) || 3;
+    const randomQuestions = getRandomQuestions(count);
+    res.json(randomQuestions);
   }
-  const saved = saveAnswer(userId, questionId, answer);
-  res.json(saved);
-};
+
+  static async postAnswer(
+    req: Request,
+    res: Response,
+  ): Promise<Response | undefined> {
+    const { userId, questionId, answer } = req.body;
+    if (!userId || !questionId || !answer) {
+      return res.status(400).json({ error: "Brakuje danych" });
+    }
+    const saved = await saveAnswer(userId, questionId, answer);
+    return res.json(saved);
+  }
+}
