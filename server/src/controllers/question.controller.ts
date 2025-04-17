b117a0b78e815fc08e8823ce48007805f92eeb6e
hkaczyns <01187235@pw.edu.pl>
2025-04-17 16:24:07 +0200

    Added methods for getting answers from db to QuestionController and router

--- server/src/controllers/question.controller.ts
+++ server/src/controllers/question.controller.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
-import { getRandomQuestions, saveAnswer } from "../services/question.service";
+import {
+  getRandomQuestions,
+  saveAnswer,
+  getAllAnswers,
+  getAnswersByQuestion,
+  getAnswersByUser,
+} from "../services/question.service";
 
 export class QuestionController {
   static async getRandom(req: Request, res: Response): Promise<void> {
@@ -18,5 +24,29 @@ export class QuestionController {
     }
     const saved = await saveAnswer(userId, questionId, answer);
     return res.json(saved);
+  }
+
+  static async getAnswersByUser(
+    req: Request,
+    res: Response,
+  ): Promise<Response | undefined> {
+    const userId = req.params.userId;
+    const answers = await getAnswersByUser(userId);
+    return res.json(answers);
+  }
+  static async getAnswersByQuestion(
+    req: Request,
+    res: Response,
+  ): Promise<Response | undefined> {
+    const questionId = req.params.questionId;
+    const answers = await getAnswersByQuestion(questionId);
+    return res.json(answers);
+  }
+  static async getAllAnswers(
+    req: Request,
+    res: Response,
+  ): Promise<Response | undefined> {
+    const answers = await getAllAnswers();
+    return res.json(answers);
   }
 }
--- server/src/routers/question.router.ts
+++ server/src/routers/question.router.ts
@@ -4,6 +4,15 @@ import { QuestionController } from "../controllers/question.controller";
 const router = Router();
 
 router.get("/random", QuestionController.getRandom);
+router.get("/answers", (req, res, next) => {
+  QuestionController.getAllAnswers(req, res).catch(next);
+});
+router.get("/answers/user/:userId", (req, res, next) => {
+  QuestionController.getAnswersByUser(req, res).catch(next);
+});
+router.get("/answers/question/:questionId", (req, res, next) => {
+  QuestionController.getAnswersByQuestion(req, res).catch(next);
+});
 router.post("/answer", (req, res, next) => {
   QuestionController.postAnswer(req, res).catch(next);
 });
