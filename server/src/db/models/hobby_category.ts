31f29bd72bb4b5e5a71e22b190d925e4e1791d9b
bzelazko <01187306@pw.edu.pl>
2025-04-17 17:14:17 +0200

    hobby categories added

---
+++ server/src/db/models/hobby_category.ts
@@ -0,0 +1,38 @@
+export type HobbyCategory = {
+  id: number;
+  hobby_category_name: string;
+  hobby_category_description: string;
+};
+
+export const questions: HobbyCategory[] = [
+  {
+    id: 1,
+    hobby_category_name: "Sport",
+    hobby_category_description: "Aktywność fizyczna, rywalizacja, zdrowie.",
+  },
+  {
+    id: 2,
+    hobby_category_name: "Książki",
+    hobby_category_description: "Czytanie, literatura, rozwój osobisty.",
+  },
+  {
+    id: 3,
+    hobby_category_name: "Gry",
+    hobby_category_description: "Rozrywka, strategia, współpraca.",
+  },
+  {
+    id: 4,
+    hobby_category_name: "Podróże",
+    hobby_category_description: "Odkrywanie nowych miejsc, kultura, przygoda.",
+  },
+  {
+    id: 5,
+    hobby_category_name: "Muzyka",
+    hobby_category_description: "Słuchanie, tworzenie, emocje.",
+  },
+  {
+    id: 6,
+    hobby_category_name: "Nauka",
+    hobby_category_description: "Badania, eksperymenty, odkrycia.",
+  },
+];
