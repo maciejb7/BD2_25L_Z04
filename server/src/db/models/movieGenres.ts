77456e48a9207d216ba87dea3936a88704c5c3fa
bzelazko <01187306@pw.edu.pl>
2025-04-18 16:22:08 +0200

    Add movie genres type and initial data

---
+++ server/src/db/models/movieGenres.ts
@@ -0,0 +1,47 @@
+export type MovieGenre = {
+  id: number;
+  movie_genre_name: string;
+};
+
+export const movieGenres: MovieGenre[] = [
+  {
+    id: 1,
+    movie_genre_name: "Akcja",
+  },
+  {
+    id: 2,
+    movie_genre_name: "Komedia",
+  },
+  {
+    id: 3,
+    movie_genre_name: "Dramat",
+  },
+  {
+    id: 4,
+    movie_genre_name: "Horror",
+  },
+  {
+    id: 5,
+    movie_genre_name: "Sci-Fi",
+  },
+  {
+    id: 6,
+    movie_genre_name: "Romantyczny",
+  },
+  {
+    id: 7,
+    movie_genre_name: "Dokumentalny",
+  },
+  {
+    id: 8,
+    movie_genre_name: "Animowany",
+  },
+  {
+    id: 9,
+    movie_genre_name: "Fantasy",
+  },
+  {
+    id: 10,
+    movie_genre_name: "Thriller",
+  },
+];
