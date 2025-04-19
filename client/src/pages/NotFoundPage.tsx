79cbc668609839f44b36636544ffc0fef327caf1
alewand <lewandowskiamadeusz2004@gmail.com>
2025-04-19 15:43:18 +0200

    Added 404 Page style.

--- client/src/pages/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage.tsx
@@ -1,12 +1,24 @@
 import TopBar from "../components/common/TopBar";
+import { isAuthenticated } from "../utils/AuthInfo";
 
-const topBarOptions = [{ name: "Strona Główna", link: "/" }];
+const topBarOptions = [
+  { name: isAuthenticated() ? "DashBoard" : "Strona Główna", link: "/" },
+];
 
 function NotFoundPage() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col bg-gray-50">
       <TopBar options={topBarOptions}></TopBar>
-      <h1>404 Not Found</h1>
+      <div className="flex-grow flex flex-col items-center justify-center px-6 py-12">
+        <h1 className="text-6xl font-bold text-blue-600 mb-4">404</h1>
+        <h2 className="text-2xl font-semibold text-gray-700 mb-6">
+          STRONA NIE ZNALEZIONA
+        </h2>
+        <p className="text-gray-600 text-center max-w-lg mb-8">
+          Przepraszamy, ale strona której szukasz nie istnieje lub została
+          przeniesiona.
+        </p>
+      </div>
     </div>
   );
 }
