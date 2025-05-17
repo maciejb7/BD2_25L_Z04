2c2d93631a6b8b76a449db89dfa0528163511f65
alewand <lewandowskiamadeusz2004@gmail.com>
2025-05-17 16:23:17 +0200

    Cleared dashboard.

--- client/src/pages/DashBoardPage.tsx
+++ client/src/pages/DashBoardPage.tsx
@@ -1,46 +1,14 @@
-import { useState } from "react";
-import { deleteAccount, logout, logoutFromAllDevices } from "../api/api.auth";
-import ConfirmForm from "../components/auth/ConfirmModal";
-import { useAlert } from "../contexts/AlertContext";
-import { getAuthObserver } from "../utils/AuthObserver";
+import MainBackground from "../components/common/MainBackground";
+
 
 function DashboardPage() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const { showAlert } = useAlert();
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard!</p>
-      <button
-        onClick={async () => {
-          const response = await logout();
-          getAuthObserver().emitLogout(response.message, "success");
-        }}
-      >
-        Wyloguj
-      </button>
-      <button
-        onClick={async () => {
-          try {
-            await logoutFromAllDevices();
-          } catch (error: any) {
-            showAlert(error.message, "error");
-          }
-        }}
-      >
-        Wyloguj ze wszystkich urządzeń
-      </button>
-      <button onClick={() => setIsModalOpen(true)}>Usuń konto</button>
-
-      <ConfirmForm
-        apiCall={deleteAccount}
-        modalTitle={"Potwierdź usunięcie konta"}
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-      />
-    </div>
-  );
+  <div className="flex flex-col items-center justify-center">
+    <MainBackground blur="80">
+    </MainBackground>
+  </div>
+);
 }
 
 export default DashboardPage;
