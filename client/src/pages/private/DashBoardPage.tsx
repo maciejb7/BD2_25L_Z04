97899629872bb3e24c3f23d20bd5d384843af55d
alewand <lewandowskiamadeusz2004@gmail.com>
2025-06-11 20:55:51 +0200

    Updated dashboard.

--- client/src/pages/private/DashBoardPage.tsx
+++ client/src/pages/private/DashBoardPage.tsx
@@ -1,4 +1,5 @@
 import Background from "../../components/common/Background";
+import Logo from "../../components/common/Logo";
 import SideBar from "../../components/common/SideBar";
 import { getSideBarOptions } from "../../constants/sideBarOptions";
 
@@ -7,7 +8,16 @@ function DashboardPage() {
     <div className="flex flex-col items-center justify-center">
       <Background blur="lg">
         <SideBar options={getSideBarOptions("Dashboard")} />
-        <div className="ml-12 sm:ml-16"></div>
+        <div className="flex flex-col items-center justify-center ml-12 sm:ml-16 h-screen">
+          <Logo size="xl" />
+          <h1 className="text-xl sm:text-2xl md:text-3xl font-bold mt-6 text-white drop-shadow-xl">
+            WITAJ!
+          </h1>
+          <h2 className="text-lg sm:text-xl md:text-2xl font-semibold mt-4 text-white drop-shadow-lg">
+            Przejdź do menu po lewej stronie, aby rozpocząć korzystanie z
+            aplikacji.
+          </h2>
+        </div>
       </Background>
     </div>
   );
